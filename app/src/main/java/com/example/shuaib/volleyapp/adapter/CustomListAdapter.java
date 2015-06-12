ed4f7a61f2d51b8f1d205f6443fac7cdb4232d78
MOHAMMED SAIF <imsaif4u@gmail.com>
2015-06-12 23:00:13 +0530

    Initial commit

---
+++ app/src/main/java/com/example/shuaib/volleyapp/adapter/CustomListAdapter.java
@@ -0,0 +1,99 @@
+package com.example.shuaib.volleyapp.adapter;
+import java.util.List;
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.android.volley.toolbox.ImageLoader;
+import com.android.volley.toolbox.NetworkImageView;
+
+import com.example.shuaib.volleyapp.AppController.AppController;
+import com.example.shuaib.volleyapp.MainActivity;
+import com.example.shuaib.volleyapp.R;
+import com.example.shuaib.volleyapp.model.Movie;
+
+import java.util.List;
+
+/**
+ * Created by Shuaib on 6/11/2015.
+ */
+public class CustomListAdapter extends BaseAdapter {
+    private Activity activity;
+    private LayoutInflater inflater;
+    private List<Movie> movieItems;
+    ImageLoader imageLoader = AppController.getInstance().getImageLoader();
+
+    public CustomListAdapter(Activity activity, List<Movie> movieItems) {
+        this.activity = activity;
+        this.movieItems = movieItems;
+    }
+
+    @Override
+    public int getCount() {
+        return movieItems.size();
+    }
+
+    @Override
+    public Object getItem(int location) {
+        return movieItems.get(location);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        if (inflater == null)
+            inflater = (LayoutInflater) activity
+                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        if (convertView == null)
+            convertView = inflater.inflate(R.layout.list_row, null);
+
+        if (imageLoader == null)
+            imageLoader = AppController.getInstance().getImageLoader();
+        NetworkImageView thumbNail = (NetworkImageView) convertView
+                .findViewById(R.id.thumbnail);
+        TextView title = (TextView) convertView.findViewById(R.id.title);
+        TextView rating = (TextView) convertView.findViewById(R.id.rating);
+        TextView genre = (TextView) convertView.findViewById(R.id.genre);
+        TextView year = (TextView) convertView.findViewById(R.id.releaseYear);
+        Button button = (Button) convertView.findViewById(R.id.button);
+
+
+        // getting movie data for the row
+        Movie m = movieItems.get(position);
+
+        // thumbnail image
+        thumbNail.setImageUrl(m.getThumbnailUrl(), imageLoader);
+
+        // title
+        title.setText(m.getTitle());
+
+        // rating
+        rating.setText("Rating: " + String.valueOf(m.getRating()));
+
+        // genre
+        String genreStr = "";
+        for (String str : m.getGenre()) {
+            genreStr += str + ", ";
+        }
+        genreStr = genreStr.length() > 0 ? genreStr.substring(0,
+                genreStr.length() - 2) : genreStr;
+        genre.setText(genreStr);
+
+        // release year
+        year.setText(String.valueOf(m.getYear()));
+
+        return convertView;
+    }
+
+}
